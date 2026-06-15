582888de9321673454aa9a3184e61a2564b0ce08
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-15 20:57:08 -0300

    feat: create Menu and Game classes

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from Game import Game
 
-print("Setup start")
-pygame.init()
-window = pygame.display.set_mode(size=(600,480))
-print("Setup finish")
-
-print("Loop start")
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
