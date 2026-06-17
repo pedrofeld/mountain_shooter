931d26bac7fa06c4fc7689687beaf89d29c0ff8c
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-17 20:56:04 -0300

    feat: create Background class (images passing to give movement)

---
+++ Background.py
@@ -0,0 +1,8 @@
+from Entity import Entity
+
+class Background(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self, ):
+        pass
