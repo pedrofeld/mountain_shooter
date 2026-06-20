dc4587d878cd95698e283a8890f6c152efb52f56
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-20 17:25:15 -0300

    feat: define parameters and create constructor

--- code/Player.py
+++ code/Player.py
@@ -1,8 +1,8 @@
 from code import Entity
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def update(self, ):
         pass
