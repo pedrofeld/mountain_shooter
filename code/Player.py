4444ce3db192e5a324d9c94f859880131c66fd92
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-20 17:43:46 -0300

    feat: move the user upwards to the edge of the screen

--- code/Player.py
+++ code/Player.py
@@ -1,3 +1,5 @@
+import pygame
+
 from code.Entity import Entity
 
 
@@ -5,8 +7,11 @@ class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
 
-    def update(self, ):
-        pass
+    # def update(self, ):
+    #     pass
 
     def move(self, ):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_UP] and self.rect.top > 0:
+            self.rect.centery -= 1
         pass
