6caefca335500bc20b6c0b52ef923411127971ea
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-20 17:14:17 -0300

    feat: add convert_alpha() to improve performance and to preserve transparency

--- code/Menu.py
+++ code/Menu.py
@@ -8,7 +8,7 @@ from code.Const import COLOR_ORANGE, WIN_WIDTH, MENU_OPTION, COLOR_YELLOW, COLOR
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load("assets/MenuBg.png")
+        self.surf = pygame.image.load("assets/MenuBg.png").convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
