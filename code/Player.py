380db9464d20d48d78614f920a00885147f91f78
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-20 17:49:46 -0300

    feat: move the user down to the edge of the screen

--- code/Player.py
+++ code/Player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from code.Const import ENTITY_SPEED
+from code.Const import ENTITY_SPEED, WIN_HEIGHT
 from code.Entity import Entity
 
 
@@ -15,4 +15,6 @@ class Player(Entity):
         pressed_key = pygame.key.get_pressed()
         if pressed_key[pygame.K_UP] and self.rect.top > 0:
             self.rect.centery -= ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_DOWN] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
         pass
