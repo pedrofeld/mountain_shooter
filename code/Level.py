cc0d3693dbcd82bf4234ff45123177f40e342bfe
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-26 07:29:10 -0300

    feat: make score persist across levels

--- code/Level.py
+++ code/Level.py
@@ -15,20 +15,24 @@ from code.Player import Player
 
 
 class Level:
-    def __init__(self, window, name, game_mode):
+    def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
         self.timeout = TIMEOUT_LEVEL
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg'))
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        player = (EntityFactory.get_entity('Player1'))
+        player.score = player_score[0]
+        self.entity_list.append(player)
         if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2'))
+            player = (EntityFactory.get_entity('Player2'))
+            player.score = player_score[0]
+            self.entity_list.append(player)
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
         pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
 
-    def run(self):
+    def run(self, player_score: list[int]):
         pygame.mixer_music.load(f'assets/{self.name}.mp3')
         pygame.mixer_music.play()
         clock = pygame.time.Clock()
@@ -56,6 +60,11 @@ class Level:
                 if event.type == EVENT_TIMEOUT:
                     self.timeout -= TIMEOUT_STEP
                     if self.timeout <= 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                                player_score[1] = ent.score
                         return True
 
                 found_player = False
