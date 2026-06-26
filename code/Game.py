94f039dc1fc629473c98da8a8c013874fa9e6625
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-26 07:56:01 -0300

    feat: add functionality to option SCORE in the menu

--- code/Game.py
+++ code/Game.py
@@ -3,6 +3,8 @@ import pygame
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
+
 
 class Game:
     def __init__(self):
@@ -11,6 +13,7 @@ class Game:
 
     def run(self):
         while True:
+            score: Score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -21,6 +24,10 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
