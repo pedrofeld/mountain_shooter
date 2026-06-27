5b179ab87535982947db2f0f1508b42ff88d5d7a
pedrofeld <pedro.gael.feld@gmail.com>
2026-06-27 08:22:26 -0300

    feat: SQLite3 implementation for score using proxy design pattern

--- code/Score.py
+++ code/Score.py
@@ -1,8 +1,12 @@
+import datetime
+import sys
+
 import pygame
-from pygame import Surface, Rect
+from pygame import Surface, Rect, KEYDOWN
+from pygame.constants import K_RETURN, K_BACKSPACE, K_ESCAPE
 from pygame.font import Font
 
-from code.Const import C_YELLOW, SCORE_POS, MENU_OPTION
+from code.Const import C_YELLOW, SCORE_POS, MENU_OPTION, C_WHITE
 from code.DBProxy import DBProxy
 
 
@@ -16,11 +20,40 @@ class Score:
         pygame.mixer_music.load('assets/Score.mp3')
         pygame.mixer_music.play(-1)
         db_proxy = DBProxy('DBScore')
+        name = ''
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.score_text(48, 'YOU WIN!!', C_YELLOW, SCORE_POS['Title'])
             if game_mode == MENU_OPTION[0]:
-                text = 'Player 1 enter your name (4 characters):'
+                score = player_score[0]
+                text = 'Enter Player 1 name (4 characters):'
+            if game_mode == MENU_OPTION[1]:
+                score = (player_score[0] + player_score[1]) / 2
+                text = 'Enter Team name (4 characters):'
+            if game_mode == MENU_OPTION[2]:
+                if player_score[0] >= player_score[1]:
+                    score = player_score[0]
+                    text = 'Enter Player 1 name (4 characters):'
+                else:
+                    score = player_score[1]
+                    text = 'Enter Player 2 name (4 characters):'
+            self.score_text(20, text, C_WHITE, SCORE_POS['EnterName'])
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_RETURN and len(name) == 4:
+                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                        return
+                    elif event.key == K_BACKSPACE:
+                        name = name[:-1]
+                    else:
+                        if len(name) < 4:
+                            name += event.unicode
+            self.score_text(20, name, C_WHITE, SCORE_POS['Name'])
             pygame.display.flip()
             pass
 
@@ -28,7 +61,25 @@ class Score:
         pygame.mixer_music.load('assets/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
+        self.score_text(48, 'TOP 10 SCORE', C_YELLOW, SCORE_POS['Title'])
+        self.score_text(20, 'NAME         SCORE        DATE          ', C_YELLOW, SCORE_POS['Label'])
+        db_proxy = DBProxy('DBScore')
+        list_score = db_proxy.retrieve_top10()
+        db_proxy.close()
+
+        for player_score in list_score:
+            id, name, score, date = player_score
+            self.score_text(20, f'{name}       {score :05d}        {date}          ', C_YELLOW,
+                            SCORE_POS[list_score.index(player_score)])
+
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        return
             pygame.display.flip()
             pass
 
@@ -37,3 +88,9 @@ class Score:
         text_sur: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_sur.get_rect(center=text_center_pos)
         self.window.blit(source=text_sur, dest=text_rect)
+
+def get_formatted_date():
+    current_datetime = datetime.now()
+    current_time = current_datetime.strftime("%H:%M")
+    current_date = current_datetime.strftime("%d/%m/%Y")
+    return f'{current_time} - {current_date}'
